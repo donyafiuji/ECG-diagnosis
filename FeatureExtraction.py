666eb53214371e8db757543b12a9bb95cdaf9c80
donya <fiujidonya@gmail.com>
2023-06-29 09:12:53 +0430

    modified:   FeatureExtraction.py 	modified:   Preprocessing.py 	modified:   __pycache__/Preprocessing.cpython-39.pyc 	modified:   main.ipynb

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -1,7 +1,10 @@
 import numpy as np
 from scipy.signal import cwt, find_peaks
 import matplotlib.pyplot as plt
-import pywavelets as pw
+import pywt as pw
+import random
+
+
 
 
 
@@ -9,67 +12,78 @@ import pywavelets as pw
 class QRS(): 
 
 
-    # def CWT(self, signal):
+
+
+    def __init__(self):
         
-    #   sig  = signal
-    #   scales = np.arange(1, 100)
-    #   wavelet = 'morl'  
-    #   coefficients = cwt(sig, wavelet, scales)
-
-    #   widths = np.arange(1, 31)
-
-    #   cwtmatr = signal.cwt(sig, signal.ricker, widths)
+        plt.figure(figsize=(10, 2))
 
 
-    def QRS_detection(signal,sample_rate=100,max_bpm):
+    # wavelet transform, thresholding, and peak detection to extract R-peaks
+    
+    def peak_detection(self, sig):
 
-    ## Stationary Wavelet Transform 
-    coeffs = pw.swt(signal, wavelet = "haar", level=2, start_level=0, axis=-1)
-    d2 = coeffs[1][1] ##2nd level detail coefficients
-    
-    
-    ## Threhold the detail coefficients
-    avg = np.mean(d2)
-    std = np.std(d2)
-    sig_thres = [abs(i) if abs(i)>2.0*std else 0 for i in d2-avg]
-    
-    ## Find the maximum modulus in each window
-    window = int((60.0/max_bpm)*sample_rate)
-    sig_len = len(signal)
-    n_windows = int(sig_len/window)
-    modulus,qrs = [],[]
-    
-    ##Loop through windows and find max modulus
-    for i in range(n_windows):
-        start = i*window
-        end = min([(i+1)*window,sig_len])
-        mx = max(sig_thres[start:end])
-        if mx>0:
-            modulus.append( (start + np.argmax(sig_thres[start:end]),mx))
-    
-    
-    ## Merge if within max bpm
-    merge_width = int((0.2)*sample_rate)
-    i=0
-    while i < len(modulus)-1:
-        ann = modulus[i][0]
-        if modulus[i+1][0]-modulus[i][0] < merge_width:
-            if modulus[i+1][1]>modulus[i][1]: # Take larger modulus
-                ann = modulus[i+1][0]
-            i+=1
-                
-        qrs.append(ann)
-        i+=1 
-    ## Pin point exact qrs peak
-    window_check = int(sample_rate/6)
-    #signal_normed = np.absolute((signal-np.mean(signal))/(max(signal)-min(signal)))
-    r_peaks = [0]*len(qrs)
-    
-    for i,loc in enumerate(qrs):
-        start = max(0,loc-window_check)
-        end = min(sig_len,loc+window_check)
-        wdw = np.absolute(signal[start:end] - np.mean(signal[start:end]))
-        pk = np.argmax(wdw)
-        r_peaks[i] = start+pk
         
-    return r_peaks
+
+        # Computation the wavelet coefficients using the Stationary Wavelet Transform
+        coeffs = pw.swt(sig, wavelet = "db4", level=3, start_level=0, axis=-1)
+        d3 = coeffs[2][1] ##2nd level detail coefficients
+
+        sample_rate = 100
+        max_bpm = 66 
+
+
+        ## Threhold the detail coefficients
+        avg = np.mean(d3)
+        std = np.std(d3)
+        sig_thres = [abs(i) if abs(i)>4.0*std else 0 for i in d3-avg]
+
+        ## Find the maximum modulus in each window
+        window = int((60.0/max_bpm)*sample_rate)
+        sig_len = len(sig)
+        n_windows = int(sig_len/window)
+        modulus,qrs = [],[]
+
+        ##Loop through windows and find max modulus
+        for i in range(n_windows):
+            start = i*window
+            end = min([(i+1)*window,sig_len])
+            mx = max(sig_thres[start:end])
+            if mx>0:
+                modulus.append( (start + np.argmax(sig_thres[start:end]),mx))
+
+
+        ## Merge if within max bpm
+        merge_width = int((0.2)*sample_rate)
+        i=0
+        while i < len(modulus)-1:
+            ann = modulus[i][0]
+            if modulus[i+1][0]-modulus[i][0] < merge_width:
+                if modulus[i+1][1]>modulus[i][1]: # Take larger modulus
+                    ann = modulus[i+1][0]
+                i+=1
+                    
+            qrs.append(ann)
+            i+=1 
+
+        
+        ## Pin point exact qrs peak
+        window_check = int(sample_rate/6)
+        #signal_normed = np.absolute((signal-np.mean(signal))/(max(signal)-min(signal)))
+        r_peaks = []
+
+        for loc in qrs:
+            start = max(0,loc-window_check)
+            end = min(sig_len,loc+window_check)
+            wdw = np.absolute(sig[start:end] - np.mean(sig[start:end]))
+            pk = np.argmax(wdw)
+
+            if wdw[pk] >= 0.3:
+                r_peaks.append(start + pk)
+
+
+        plt.plot(sig, color='blue', label='ECG Signal')
+        plt.plot(r_peaks, sig[r_peaks], 'ro', markersize=5, label='R-Peaks')
+        plt.show()
+
+        return r_peaks
--- Preprocessing.py
+++ Preprocessing.py
@@ -2,66 +2,128 @@ import wfdb
 import numpy as np
 import pandas as pd
 import ast
+import scipy.signal as signal
+import matplotlib.pyplot as plt
+
+
+""" - Nyquist frequency is the maximum frequency that can be accurately represented
+ in a digital signal sampled at a given rate """
 
 
 
+class preprocess():
 
-
-class loadData():
 
 
     def __init__(self):
 
         self.sampling_rate=100
         self.path='/'
+        
 
 
-    def load_raw_data(self, df):
 
-        if self.sampling_rate == 100:
-            data = [wfdb.rdsamp(f) for f in df.filename_lr]
-        else:
-            data = [wfdb.rdsamp(f) for f in df.filename_hr]
+  
+    """ prepare data and split them to test and train """
 
-        data = np.array([signal for signal, meta in data])
-        meta_data = data[0][1]
-        return data, meta_data
-    
+    def loadData(self):
 
-    def example_physionet(self):
 
+        def load_raw_data(df):
+            if self.sampling_rate == 100:
+                data = [wfdb.rdsamp(f) for f in df.filename_lr]
+            else:
+                data = [wfdb.rdsamp(f) for f in df.filename_hr]
+            data = np.array([signal for signal, meta in data])
+            return data
+        
 
         # load and convert annotation data
         Y = pd.read_csv('ptbxl_database.csv', index_col='ecg_id')
         Y.scp_codes = Y.scp_codes.apply(lambda x: ast.literal_eval(x))
 
         # Load raw signal data
-        # Original_ecg, meta_data = self.load_raw_data(Y)
+        X = load_raw_data(Y)
 
-        # # Load scp_statements.csv for diagnostic aggregation
-        # agg_df = pd.read_csv('scp_statements.csv', index_col=0)
-        # agg_df = agg_df[agg_df.diagnostic == 1]
+        # Load scp_statements.csv for diagnostic aggregation
+        agg_df = pd.read_csv('scp_statements.csv', index_col=0)
+        agg_df = agg_df[agg_df.diagnostic == 1]
 
+        def aggregate_diagnostic(y_dic):
+            tmp = []
+            
+            for key in y_dic.keys():
+                if key in agg_df.index:
+                    tmp.append(agg_df.loc[key].diagnostic_class)
+            return list(set(tmp))
 
         # Apply diagnostic superclass
-        # Y['diagnostic_superclass'] = Y.scp_codes.apply(self.aggregate_diagnostic)
+        Y['diagnostic_superclass'] = Y.scp_codes.apply(aggregate_diagnostic)
 
-        # # Split data into train and test
-        # test_fold = 10
-        # # Train
-        # X_train = Original_ecg[np.where(Y.strat_fold != test_fold)]
-        # y_train = Y[(Y.strat_fold != test_fold)].diagnostic_superclass
-        # # Test
-        # X_test = X[np.where(Y.strat_fold == test_fold)]
-        # y_test = Y[Y.strat_fold == test_fold].diagnostic_superclass
+        # Split data into train and test
+        test_fold = 10
+        # Train
+        X_train = X[np.where(Y.strat_fold != test_fold)]
+        y_train = Y[(Y.strat_fold != test_fold)].diagnostic_superclass
+        # Test
+        X_test = X[np.where(Y.strat_fold == test_fold)]
+        y_test = Y[Y.strat_fold == test_fold].diagnostic_superclass
 
-        return Y,Y.scp_codes
+        return X_train, y_train, X_test, y_test
 
+
+
+
+
+    """  baseline wander removal = low-frequency drift """
+
+    def highpassfilter(self, origin_signal ,sampling_rate , filter_order, cutoff_freq):
+
+        nyquist_freq = 0.5 * sampling_rate
+        cutoff_freq = cutoff_freq 
+        filter_order = filter_order
+
+        # Design a Butterworth highpass filter
+        b, a = signal.butter(filter_order, cutoff_freq / nyquist_freq, btype='highpass')
+
+
+        # Apply the filter to the ECG signal
+        baseline_removed_ecg = signal.filtfilt(b, a, origin_signal)
+
+        
+        return baseline_removed_ecg 
     
-    def aggregate_diagnostic(self, y_dic):
-        tmp = []
-        for key in y_dic.keys():
-            if key in self.agg_df.index:
-                tmp.append(self.agg_df.loc[key].diagnostic_class)
-        return list(set(tmp))
+
+
+
+
+
+    def bandpassfilter(self , origin_signal, sampling_rate ,filter_order ,lowcut_freq , highcut_freq): 
+
+
+        highcut = highcut_freq
+        lowcut = lowcut_freq 
+        filter_order = filter_order  
+        nyquist_freq = 0.5 * sampling_rate
+        b, a = signal.butter(filter_order, [lowcut / nyquist_freq, highcut / nyquist_freq], btype='band')
+
+        bandpassfiltered = signal.filtfilt(b, a, origin_signal)
+   
+        # plt.plot(origin_signal, label= 'original signal')
+        # plt.plot(bandpassfiltered, label = 'bandpass filtered')
+        # plt.show()
+
+        return bandpassfiltered
     
+
+
+
+    def movingaveragefilter(self ,original_signal):
+
+        MovingAvarage_filtered = np.convolve(original_signal, np.ones((3,))/3, mode="same")
+
+        # plt.plot(original_signal, label= 'original signal')
+        # plt.plot(MovingAvarage_filtered, label = 'bandpass filtered')
+        # plt.show()
+
+        return MovingAvarage_filtered
